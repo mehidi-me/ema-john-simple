d22e27f9b4cae0c771d0799aad33004a7ce00e99
Mahedi Hasan <mahedihasan080080@gmail.com>
2020-08-25 07:51:01 +0600

    f

---
+++ src/componets/Shop/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>hello i am a shop</h1>
+        </div>
+    );
+};
+
+export default Shop;
